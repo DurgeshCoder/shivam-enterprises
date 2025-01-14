fe3332ad9eedcc0ce741188cbd517284ed86d3c7
batchlcwd <batchlcwd@gmail.com>
2025-01-14 12:21:13 +0530

    theme dark default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light">
+    <html lang="en" className="dark">
       <body className={`bg-gray-100 dark:bg-gray-900`}>
         <CustomNavbar />
         <Flowbite>{children}</Flowbite>
